816bb1780b68ba2e27b097a09840da374be57512
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-11 16:05:53 +0500

    Task 3: Add a First Model: Product

--- NetShop/Models/Product.cs
+++ NetShop/Models/Product.cs
@@ -3,5 +3,15 @@
 	public class Product
 	{
 		public int Id { get; set; }
+
+		public string Name { get; set; }
+
+		public string Image { get; set; }
+
+		public int Price { get; set; }
+
+		public int ShortDesc { get; set; }
+
+		public string LongDesc { get; set; }
 	}
 }
