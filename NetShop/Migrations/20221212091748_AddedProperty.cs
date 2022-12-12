7590e6bb2cf45ce97d0bb980db2c92e2ccada7fe
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-12 14:20:56 +0500

    Task 11: Added a table of properties to database

---
+++ NetShop/Migrations/20221212091748_AddedProperty.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NetShop.Migrations
+{
+    public partial class AddedProperty : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Properties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Properties", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Properties_Id",
+                table: "Properties",
+                column: "Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Properties");
+        }
+    }
+}
---
+++ NetShop/Models/Configuration/PropertyConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NetShop.Models.Configuration
+{
+    public class PropertyConfiguration : IEntityTypeConfiguration<Property>
+    {
+        public void Configure(EntityTypeBuilder<Property> builder)
+        {
+            builder.ToTable("Properties").HasKey(b => b.Id);
+            builder.HasIndex(b => b.Id).IsUnique();
+            builder.Property(b => b.Value).IsRequired();
+        }
+    }
+}
