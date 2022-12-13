3f3ded57bc4a2ee89b55e94914211d37eb628fb0
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-13 14:09:16 +0500

    Task 14: Add a characteristics model to database

---
+++ NetShop/Migrations/20221213090819_AddAcharacteristicsModel.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace NetShop.Migrations
+{
+    public partial class AddAcharacteristicsModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ProductProperties",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ProductId = table.Column<int>(type: "integer", nullable: false),
+                    PropertyId = table.Column<int>(type: "integer", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductProperties", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProductProperties_Id",
+                table: "ProductProperties",
+                column: "Id",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProductProperties");
+        }
+    }
+}
---
+++ NetShop/Models/Configuration/ProductPtopertyConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NetShop.Models.Configuration
+{
+    public class ProductPtopertyConfiguration : IEntityTypeConfiguration<ProductProperty>
+    {
+        public void Configure(EntityTypeBuilder<ProductProperty> builder)
+        {
+            builder.ToTable("ProductProperties").HasKey(b => b.Id);
+            builder.HasIndex(b => b.Id).IsUnique();
+            builder.Property(b => b.PropertyId).IsRequired();
+            builder.Property(b => b.ProductId).IsRequired();
+            builder.Property(b => b.Value).IsRequired();
+        }
+    }
+}
