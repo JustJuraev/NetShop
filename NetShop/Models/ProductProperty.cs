ae18d932c214441f461e083c2f9a4cf2bbb70018
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-13 13:31:13 +0500

    Task 12: Add a model for characteristics

---
+++ NetShop/Models/ProductProperty.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NetShop.Models
+{
+    public class ProductProperty
+    {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [NotMapped]
+        public Product Product { get; set; }
+
+        public int PropertyId { get; set; }
+
+        [NotMapped]
+        public Property Property { get; set; }
+
+        public string Value { get; set; }
+    }
+}
