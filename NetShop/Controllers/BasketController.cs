7171804973740e4a8f4746c13f709c889531ac28
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-15 15:45:52 +0500

    Task: 19, 20, 21: Add a controller and a class for basket

---
+++ NetShop/Controllers/BasketController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetShop.Models;
+using NetShop.Service.Interfaces;
+using System.Linq;
+
+namespace NetShop.Controllers
+{
+    public class BasketController : Controller
+    {
+        private IProductService _productService;
+
+        public BasketController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        public string GetSessionId()
+        {
+            HttpContext.Session.SetString("UserKey", "Product");
+            TempData["Session"] = HttpContext.Session.Id;
+            return TempData["Session"].ToString();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(int id)
+        {
+            BasketList.Add(GetSessionId(), _productService.GetAll().FirstOrDefault(x => x.Id == id));
+            return RedirectToAction("Index");
+        }
+        public IActionResult Index()
+        {
+            return View(BasketList.GetBasket(GetSessionId()));
+        }
+    }
+}
---
+++ NetShop/Models/BasketList.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace NetShop.Models
+{
+    public static class BasketList
+    {
+        private static Dictionary<string, List<Product>> Basket = new Dictionary<string, List<Product>>();
+
+        public static List<Product> GetBasket(string sessionId)
+        {
+           
+            if (!Basket.ContainsKey(sessionId))
+            {
+                return new List<Product>();
+            }
+
+            return Basket[sessionId];
+        }
+
+        public static void Add(string sessionId, Product product)
+        {
+            string test = sessionId;
+            if (!Basket.ContainsKey(sessionId))
+            {
+                Basket.Add(sessionId, new List<Product>());
+            }
+            
+            Basket[sessionId].Add(product);
+        }
+
+
+        public static void Remove(string sessionId, Product product)
+        {
+            Basket[sessionId].Remove(product);
+        }
+    }
+}
