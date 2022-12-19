964fad15b5df2ee5c8fa4dd40e90bf733c650c0d
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-19 16:53:08 +0500

    Task 26, 27: Added a Order Model

---
+++ NetShop/Models/Configuration/OrderConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NetShop.Models.Configuration
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.ToTable("Orders").HasIndex(x => x.Id);
+            builder.HasIndex(b => b.Id).IsUnique(); 
+            builder.Property(b => b.Address).IsRequired();
+            builder.Property(b => b.Number).IsRequired();
+            builder.Property(b => b.Delivery).IsRequired();
+            builder.Property(b => b.Date).IsRequired();
+        }
+    }
+}
---
+++ NetShop/Models/Order.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NetShop.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+
+        public string Address { get; set; }
+
+        public string Number { get; set; }
+
+        public bool Delivery { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
