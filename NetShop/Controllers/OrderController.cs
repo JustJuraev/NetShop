0e852fcbe700b60f376933271fe515273ba38e01
Abdusamad Juraev <juraevabdusamad517@gmail.com>
2022-12-20 16:37:12 +0500

    Task 29, 30: Added a controller for order and view

---
+++ NetShop/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetShop.Models;
+using NetShop.Service.Interfaces;
+using System.Linq;
+
+namespace NetShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        public string GetSessionId()
+        {
+            HttpContext.Session.SetString("UserKey", "Product");
+            TempData["Session"] = HttpContext.Session.Id;
+            return TempData["Session"].ToString();
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Basket = BasketList.GetBasket(GetSessionId()).Select(x => x.Name).ToList();
+            ViewBag.Basket2 = BasketList.GetBasket(GetSessionId());
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Order order)
+        {
+            order.Basket = BasketList.GetBasket(GetSessionId()).Select(x => x.Name).ToList();
+            _orderService.Create(order);
+            return RedirectToAction("Index", "Product");
+        }
+
+        
+
+
+    }
+}
